dae623ccfa9ac1f911657a465811aeaafa61d40c
sheadawson <shea@livesource.co.nz>
2015-09-28 15:36:39 +1000

    update default env settings

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ RED="\033[0;31m"
 YELLOW="\033[0;33m"  
 GREEN="\033[0;32m"    
  
-# Set default variables
+# Set default environment variables
 
 CONFIG_DIR="~/deploy/config/"
 PHP_EXEC="/usr/bin/php-cli"
